3af31525bc41af63e1ea11fa510c3b1cad4b408f
Bastien <bastien.hugel@reseau.eseo.fr>
2023-10-13 16:48:25 +0200

    change on the page

--- front/src/routes/productCreation/+page.svelte
+++ front/src/routes/productCreation/+page.svelte
@@ -58,71 +58,150 @@
 			text: newTypeName
 		}];
         newTypeName = '';
-        showAddTypeInput = false;
+        showAddTypeInput = !showAddTypeInput;
 	}
 
 	let selectedCategorie;
     let selectedType;
 
 	function handleSubmit() {
-		
+		alert(
+			`plat ${name} (${description}) à ${price}€ avec le type: ${selectedType} et la catégorie: ${selectedCategorie}`
+		);
 	}
 </script>
 
-<form on:submit|preventDefault={handleSubmit}>
-    <input type="text" id="name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Name" required>
+<main class="centered">
+    <form on:submit|preventDefault={handleSubmit}>
 
-    <input type="text" id="description" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Description" required>
+        <h2>Catégorie</h2>
 
-    <input type="text" id="price" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Prix" required>
+        <select bind:value={selectedCategorie}
+        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
+        >
+            {#each categories as categorie}
+                <option value={categorie.text}>
+                    {categorie.text}
+                </option>
+            {/each}
+        </select>
+        
+        {#if !showAddCategoryInput}
+            <button on:click={() => showAddCategoryInput = true}
+                class="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                Ajouter
+            </button>
+        {/if}
+        
+        {#if showAddCategoryInput}
+        <div>
+            <input type="text" bind:value={newCategoryName} 
+            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
+            placeholder="Nouvelle catégorie" 
+        />
+            <button on:click={addCategory}
+            class="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                Ajouter
+            </button>
+        </div>
+        {/if}
 
-    <h2>Catégorie</h2>
+        <h2>Type</h2>
 
-	<select bind:value={selectedCategorie}>
-		{#each categories as categorie}
-			<option value={categorie.id}>
-				{categorie.text}
-			</option>
-		{/each}
-	</select>
-    
-    <button on:click={() => showAddCategoryInput = true}>
-        Ajouter
-    </button>
-    
-    {#if showAddCategoryInput}
-    <div>
-        <input type="text" bind:value={newCategoryName} />
-        <button on:click={addCategory}>
-            Ajouter
+        <select bind:value={selectedType}
+        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
+        >
+            {#each types as type}
+                <option value={type.text}>
+                    {type.text}
+                </option>
+            {/each}
+        </select>
+
+        {#if !showAddTypeInput}
+            <button on:click={() => showAddTypeInput = !showAddTypeInput}
+                class="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                Ajouter
+            </button>
+        {/if}
+        
+        {#if showAddTypeInput}
+        <div>
+            <input 
+                type="text" 
+                bind:value={newTypeName} 
+                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
+                placeholder="Nouveau type" 
+            />
+            <button on:click={addType}
+            class="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                Ajouter
+            </button>
+        </div>
+        {/if}
+
+        
+        <input 
+            bind:value={name}
+            type="text" 
+            id="name" 
+            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
+            placeholder="Name" 
+            required
+        />
+
+        <input 
+            bind:value={description}
+            type="text" 
+            id="description" 
+            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
+            placeholder="Description" 
+            required
+        />
+
+        <input 
+            bind:value={price}
+            type="number" 
+            id="price" 
+            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
+            placeholder="Prix €" 
+            step="0.01"
+            required
+        />
+
+        <button type="submit"
+            class="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+            Submit
         </button>
-    </div>
-    {/if}
+    </form>
+</main>
 
-    <h2>Type</h2>
+<style>
+	.centered {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		height: 100vh;
+	}
 
-    <select bind:value={selectedType}>
-		{#each types as type}
-			<option value={type.id}>
-				{type.text}
-			</option>
-		{/each}
-	</select>
-    
-    <button on:click={() => showAddTypeInput = true}>
-        Ajouter
-    </button>
-    
-    {#if showAddTypeInput}
-    <div>
-        <input type="text" bind:value={newTypeName} />
-        <button on:click={addType}>
-            Ajouter
-        </button>
-    </div>
-    {/if}
+	form {
+		display: flex;
+		flex-direction: column;
+		gap: 1rem;
+	}
 
-	<button type="submit">
-		Submit
-	</button>
-</form>
+	h1 {
+		text-align: center;
+	}
+
+	.bold {
+		font-weight: bold;
+		font-size: 30px;
+	}
+
+	u {
+		cursor: pointer;
+		text-align: center;
+	}
+</style>

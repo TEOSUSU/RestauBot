f7c54540cd4d8349c7f68147efa05932a0a85f10
Jules-Steiner <97162125+Jules-Steiner@users.noreply.github.com>
2023-10-16 11:49:51 +0200

    #commit PFE-18 #comment removal of useless import

--- back/src/main/java/com/restaubot/spring/controllers/RestaurantController.java
+++ back/src/main/java/com/restaubot/spring/controllers/RestaurantController.java
@@ -8,14 +8,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.restaubot.spring.models.dto.CustomerDTO;
 import com.restaubot.spring.models.dto.RestaurantDTO;
 import com.restaubot.spring.security.CustomRuntimeException;
 import com.restaubot.spring.services.RestaurantService;

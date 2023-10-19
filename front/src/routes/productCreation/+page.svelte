8f234e5723d72bcb35a4941fb0a3ccdbe84cbecb
Bastien <bastien.hugel@reseau.eseo.fr>
2023-10-19 14:16:05 +0200

    form validation

--- front/src/routes/productCreation/+page.svelte
+++ front/src/routes/productCreation/+page.svelte
@@ -1,15 +1,17 @@
 <script>
-	let name = '';
-	let description = '';
-	let price = '';
-    let photo = '';
+	let name;
+	let description;
+	let price;
+    let photo;
 
     export let data;
+	import { invalidateAll } from '$app/navigation';
     
 	let categories = data.allCategories;
     let types = data.allTypes;
+    let formSubmitted = false;
 
-    let newCategoryName = '';
+    let newCategoryName;
     let showAddCategoryInput = false;
 
     async function addCategory() {
@@ -25,10 +27,10 @@
 		});
         newCategoryName = '';
         showAddCategoryInput = false;
-        location.reload();
+        invalidateAll();
 	}
 
-    let newTypeName = '';
+    let newTypeName;
     let showAddTypeInput = false;
 
     async function addType() {
@@ -47,7 +49,7 @@
 		});
         newTypeName = '';
         showAddCategoryInput = false;
-        location.reload();
+        invalidateAll();
 	}
 
 	let selectedCategorie;
@@ -73,130 +75,146 @@
 			},
 			body: JSON.stringify(body)
 		});
-        location.reload();
+        formSubmitted = true;
+        name = "";
+	    description = "";
+	    price = "";
+        photo = "";
+        selectedCategorie = "";
+        selectedType = "";
+        invalidateAll();
 	}
 </script>
 
 <main class="centered">
-    <form on:submit|preventDefault={createDish}>
+    {#if !formSubmitted}
+        <form on:submit|preventDefault={createDish}>
 
-        <h2>Catégorie</h2>
+            <h2>Catégorie</h2>
 
-        <select bind:value={selectedCategorie}
-        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
-        required>
-            <option value="" disabled selected>Sélectionner une catégorie</option>
-            {#each categories as categorie}
-                <option value={categorie.idCategory}>
-                    {categorie.name}
-                </option>
-            {/each}
-        </select>
-        
-        {#if !showAddCategoryInput}
-        <button on:click={() => showAddCategoryInput = true}
-            class="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-            Ajouter
-        </button>
-    {/if}
-    
-    {#if showAddCategoryInput}
-    <div>
-        <input type="text" bind:value={newCategoryName} 
-        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
-        placeholder="Nouvelle catégorie" 
-    />
-        <button on:click={addCategory}
-        class="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-            Ajouter
-        </button>
-    </div>
-    {/if}
-
-        <h2>Type</h2>
-
-        {#if selectedCategorie}
-            <select bind:value={selectedType}
+            <select bind:value={selectedCategorie}
             class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
             required>
-                <option value="" disabled selected>Sélectionner un type</option>
-                {#each types as type}
-                    {#if type.category.idCategory === selectedCategorie}
-                        <option value={type.idType}>
-                            {type.name}
-                        </option>
-                    {/if}
+                <option value="" disabled selected>Sélectionner une catégorie</option>
+                {#each categories as categorie}
+                    <option value={categorie.idCategory}>
+                        {categorie.name}
+                    </option>
                 {/each}
             </select>
-
-            {#if !showAddTypeInput}
-                <button on:click={() => showAddTypeInput = !showAddTypeInput}
-                    class="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                    Ajouter
-                </button>
-            {/if}
             
-            {#if showAddTypeInput}
-            <div>
-                <input 
-                    type="text" 
-                    bind:value={newTypeName} 
-                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
-                    placeholder="Nouveau type" 
-                />
-                <button on:click={addType}
+            {#if !showAddCategoryInput}
+            <button on:click={() => showAddCategoryInput = true}
                 class="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                    Ajouter
-                </button>
-            </div>
-            {/if}
-        {:else}
-            <p>Veuillez choisir une catégorie</p>
+                Ajouter
+            </button>
+        {/if}
+        
+        {#if showAddCategoryInput}
+        <div>
+            <input type="text" bind:value={newCategoryName} 
+            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
+            placeholder="Nouvelle catégorie" 
+        />
+            <button on:click={addCategory}
+            class="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                Ajouter
+            </button>
+        </div>
         {/if}
 
-        
-        <input 
-            bind:value={name}
-            type="text" 
-            id="name" 
-            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
-            placeholder="Name" 
-            required
-        />
+            <h2>Type</h2>
 
-        <input 
-            bind:value={description}
-            type="text" 
-            id="description" 
-            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
-            placeholder="Description" 
-            required
-        />
+            {#if selectedCategorie}
+                <select bind:value={selectedType}
+                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
+                required>
+                    <option value="" disabled selected>Sélectionner un type</option>
+                    {#each types as type}
+                        {#if type.category.idCategory === selectedCategorie}
+                            <option value={type.idType}>
+                                {type.name}
+                            </option>
+                        {/if}
+                    {/each}
+                </select>
 
-        <input 
-            bind:value={price}
-            type="number" 
-            id="price" 
-            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
-            placeholder="Prix €" 
-            step="0.01"
-            required
-        />
+                {#if !showAddTypeInput}
+                    <button on:click={() => showAddTypeInput = !showAddTypeInput}
+                        class="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                        Ajouter
+                    </button>
+                {/if}
+                
+                {#if showAddTypeInput}
+                <div>
+                    <input 
+                        type="text" 
+                        bind:value={newTypeName} 
+                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
+                        placeholder="Nouveau type" 
+                    />
+                    <button on:click={addType}
+                    class="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                        Ajouter
+                    </button>
+                </div>
+                {/if}
+            {:else}
+                <p>Veuillez choisir une catégorie</p>
+            {/if}
 
-        <input 
-            bind:value={photo}
-            type="photo" 
-            id="photo" 
-            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
-            placeholder="Photo" 
-            required
-        />
+            
+            <input 
+                bind:value={name}
+                type="text" 
+                id="name" 
+                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
+                placeholder="Name" 
+                required
+            />
 
-        <button type="submit"
+            <input 
+                bind:value={description}
+                type="text" 
+                id="description" 
+                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
+                placeholder="Description" 
+                required
+            />
+
+            <input 
+                bind:value={price}
+                type="number" 
+                id="price" 
+                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
+                placeholder="Prix €" 
+                step="0.01"
+                required
+            />
+
+            <input 
+                bind:value={photo}
+                type="photo" 
+                id="photo" 
+                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
+                placeholder="Photo" 
+                required
+            />
+
+            <button type="submit"
+                class="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                Submit
+            </button>
+        </form>
+
+    {:else}
+        <p class="text-green-600 dark:text-green-300">Plat ajouté avec succès !</p>
+        <button on:click={() => formSubmitted = false}
             class="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-            Submit
+            Ajouter un nouveau plat
         </button>
-    </form>
+    {/if}
 </main>
 
 <style>

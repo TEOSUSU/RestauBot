78e423be14c5d1a1e2448cb9140b5323d17058c7
Emilien-Valain <emilien.valain@gmail.com>
2023-11-09 09:32:06 +0100

    ajout liste commandes customer dans un restaurant

---
+++ back/src/main/java/com/restaubot/spring/repositories/PurchaseRepository.java
@@ -0,0 +1,17 @@
+package com.restaubot.spring.repositories;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.restaubot.spring.models.entities.PurchaseEntity;
+
+public interface PurchaseRepository extends JpaRepository<PurchaseEntity, Long> {
+    @Query("SELECT p FROM PurchaseEntity p " +
+           "WHERE p.customer.id = :customerId AND p.restaurant.id = :restaurantId")
+    List<PurchaseEntity> getPurchasesByCustomerIdAndRestaurantId(
+        @Param("customerId") Integer customerId, @Param("restaurantId") Integer restaurantId);
+}
---
+++ back/src/main/java/com/restaubot/spring/services/PurchaseService.java
@@ -0,0 +1,45 @@
+package com.restaubot.spring.services;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import javax.transaction.Transactional;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.restaubot.spring.models.dto.CustomerDTO;
+import com.restaubot.spring.models.dto.PurchaseDTO;
+import com.restaubot.spring.models.dto.RestaurantDTO;
+import com.restaubot.spring.models.entities.CustomerEntity;
+import com.restaubot.spring.models.entities.PurchaseEntity;
+import com.restaubot.spring.repositories.CustomerRepository;
+import com.restaubot.spring.repositories.PurchaseRepository;
+import com.restaubot.spring.security.CustomRuntimeException;
+
+@Service
+@Transactional
+public class PurchaseService {
+     private static final Logger logger = LogManager.getLogger(CustomerService.class);
+
+    @Autowired
+    private PurchaseRepository purchaseRepository;
+    @Autowired
+    private ModelMapper modelMapper;
+
+    public List<PurchaseDTO> getPurchasesByCustomerAndRestaurant(Integer customerId, Integer restaurantId) throws CustomRuntimeException {
+        try {
+            List<PurchaseEntity> purchases = purchaseRepository.getPurchasesByCustomerIdAndRestaurantId(customerId, restaurantId);
+            return purchases.stream()
+                    .map(purchase -> modelMapper.map(purchase, PurchaseDTO.class))
+                    .collect(Collectors.toList());
+        } catch (Exception e) {
+            logger.error("Error retrieving purchases:", e);
+            throw new CustomRuntimeException(CustomRuntimeException.SERVICE_ERROR);
+        }
+    }
+}

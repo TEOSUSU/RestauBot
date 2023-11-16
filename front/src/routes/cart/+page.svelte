793b241541c844da475985793f5d534f93ba369b
baffouau <augustin.baffou@reseau.eseo.fr>
2023-11-16 11:20:52 +0100

    Edit navbar and add notifications

--- front/src/routes/cart/+page.svelte
+++ front/src/routes/cart/+page.svelte
@@ -49,15 +49,13 @@
   }
 </script>
 
-<!-- The rest of your component remains the same -->
-
 <div class="p-4">
-  <button on:click={goBack} class="mr-2 bg-white text-gray-500 px-3 py-2 rounded-full flex items-center">
-    <!-- Icône de flèche avec fill noir -->
-    <svg xmlns="http://www.w3.org/2000/svg" height="1.5em" viewBox="0 0 448 512">
-      <path fill="#8c8c8c" d="M64 32C28.7 32 0 60.7 0 96V416c0 35.3 28.7 64 64 64H384c35.3 0 64-28.7 64-64V96c0-35.3-28.7-64-64-64H64zm79 143c9.4-9.4 24.6-9.4 33.9 0l47 47 47-47c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9l-47 47 47 47c9.4 9.4 9.4 24.6 0 33.9s-24.6 9.4-33.9 0l-47-47-47 47c-9.4 9.4-24.6 9.4-33.9 0s-9.4-24.6 0-33.9l47-47-47-47c-9.4-9.4-9.4-24.6 0-33.9z"/>
+  <button on:click={goBack} class="mt-1 mb-2 mr-2 ml-2 bg-white text-gray-700 px-3 py-2 rounded-full flex items-center">
+    <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512">
+       <path fill="#6b7280" d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l192 192c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L77.3 256 246.6 86.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-192 192z"/>
     </svg>
-  </button>
+    Retour
+ </button>
   <h2 class="text-2xl font-bold mb-4">Panier</h2>
   {#if cartData.length === 0}
     <p>Le panier est vide.</p>
@@ -90,7 +88,7 @@
       <p class="text-xl font-bold">Total: {total.toFixed(2)} €</p>
     </div>
     <button class="w-full bg-green-500 text-white px-6 py-3 rounded mt-4">
-      Payer la commande
+      Finaliser la commande
     </button>
   {/if}
 </div>
--- front/src/routes/product/+page.svelte
+++ front/src/routes/product/+page.svelte
@@ -1,9 +1,10 @@
 <!-- App.svelte -->
-<script>
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11">
+	  import Swal from 'sweetalert2';
     import { onMount } from 'svelte';
-    import { goto } from '$app/navigation';
     import { sessionStorage } from '../../stores/stores.js';
     import { page } from '$app/stores'
+    import Returnbar from '../Returnbar.svelte';
 
     const url = $page.url;
     const productId = parseInt(url.searchParams.get('id'));
@@ -20,9 +21,9 @@
     };
 
     if (!import.meta.env.SSR) {
-        onMount(() => {
-        // Récupérer les données actuelles du panier depuis le stockage de session
+      onMount(() => {
         cartData = $sessionStorage || [];
+        
         const productApiUrl = `http://localhost:8080/api/dishes/${productId}`;
         fetch(productApiUrl, {
         method: 'GET',
@@ -40,7 +41,7 @@
         .catch(error => {
             console.error('Erreur lors de la récupération des détails du produit :', error);
         });
-        });
+      });
     }
 
     function addToCart(id, name, description, price, quantity) {
@@ -52,11 +53,25 @@
         cartData = [...cartData, { id, name, description, price, quantity }];
         }
 
-
         $sessionStorage = cartData;
 
-        console.log("Produit ajouté au panier :", product);
-        console.log(cartData)
+        let timerInterval;
+        Swal.fire({
+						title: 'Produit ajouté !',
+						text: 'Votre article à bien été ajouté au panier !',
+						icon: 'success',
+            showConfirmButton: false,
+            timer: 3000,
+            timerProgressBar: true,
+            willClose: () => {
+              clearInterval(timerInterval);
+            }
+          }).then((result) => {
+            /* Read more about handling dismissals below */
+            if (result.dismiss === Swal.DismissReason.timer) {
+              console.log("I was closed by the timer");
+            }
+					});
     }
 
   function increaseQuantity() {
@@ -68,38 +83,11 @@
       product.quantity -= 1;
     }
   }
-
-  function goBack() {
-    window.history.back();
-  }
-
-  function goToCart() {
-   goto(`/cart`, true) 
-    }
 </script>
 
-<style>
-  /* Ajoutez ici vos styles Tailwind CSS ou personnels */
-</style>
+<Returnbar {cartData} />
 
-<div class="p-4">
-  <div class="flex mb-4">
-    <button on:click={goBack} class="mr-2 bg-white text-gray-500 px-3 py-2 rounded-full flex items-center">
-      <svg xmlns="http://www.w3.org/2000/svg" height="1.5em" viewBox="0 0 448 512">
-        <path fill="#8c8c8c" d="M64 32C28.7 32 0 60.7 0 96V416c0 35.3 28.7 64 64 64H384c35.3 0 64-28.7 64-64V96c0-35.3-28.7-64-64-64H64zm79 143c9.4-9.4 24.6-9.4 33.9 0l47 47 47-47c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9l-47 47 47 47c9.4 9.4 9.4 24.6 0 33.9s-24.6 9.4-33.9 0l-47-47-47 47c-9.4 9.4-24.6 9.4-33.9 0s-9.4-24.6 0-33.9l47-47-47-47c-9.4-9.4-9.4-24.6 0-33.9z"/>
-      </svg>
-    </button>
-    
-    <button on:click={goToCart} class="ml-auto mr-2 bg-green-500 text-white rounded-full px-3 py-2 flex items-center">
-      <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 576 512">
-        <path fill="#ffffff" d="M0 24C0 10.7 10.7 0 24 0H69.5c22 0 41.5 12.8 50.6 32h411c26.3 0 45.5 25 38.6 50.4l-41 152.3c-8.5 31.4-37 53.3-69.5 53.3H170.7l5.4 28.5c2.2 11.3 12.1 19.5 23.6 19.5H488c13.3 0 24 10.7 24 24s-10.7 24-24 24H199.7c-34.6 0-64.3-24.6-70.7-58.5L77.4 54.5c-.7-3.8-4-6.5-7.9-6.5H24C10.7 48 0 37.3 0 24zM128 464a48 48 0 1 1 96 0 48 48 0 1 1 -96 0zm336-48a48 48 0 1 1 0 96 48 48 0 1 1 0-96z"/>
-      </svg>
-      {#if cartData.length > 0}
-      <span class="absolute top-2 right-4 bg-red-500 text-white rounded-full px-2 py-1 text-xs">{cartData.length}</span>
-      {/if}
-    </button>
-  </div>
-
+<div class="p-4 sm:ml-64">
   <img src={product.image} alt={product.name} class="w-full h-64 object-cover mb-4" />
 
   <h2 class="text-2xl font-bold mb-2">{product.name}</h2>

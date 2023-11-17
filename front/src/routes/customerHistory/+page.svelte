e1a5881b1a6713c20d1d797322a2b290461b2ab3
Emilien-Valain <emilien.valain@gmail.com>
2023-11-17 13:51:30 +0100

    gitignore resources + better css history

--- front/src/routes/customerHistory/+page.svelte
+++ front/src/routes/customerHistory/+page.svelte
@@ -2,7 +2,7 @@
     import { page } from '$app/stores';
     import { onMount } from "svelte";
     import { writable } from 'svelte/store';
-    import Navbar from './Navbar.svelte';
+    import Navbar from '../Navbar.svelte';
     let open = false;
     const url = $page.url;
     const customerId = parseInt(url.searchParams.get('customer'));
@@ -35,8 +35,8 @@ onMount(() => {
   <h1 class="font-bold text-xl pb-5">Historique des commandes</h1>
   <div class="overflow-x-auto shadow-md rounded-lg">
   <table class="table-auto border-collapse ">
-    <thead>
-      <tr class="bg-gray-300">
+    <thead class="">
+      <tr class="bg-slate-300 ">
         <th class="px-4 py-2">Date</th>
         <th class="px-4 py-2">Restaurant</th>
         <th class="px-4 py-2">Total</th>
@@ -44,10 +44,10 @@ onMount(() => {
     </thead>
     <tbody>
       {#each historyData as order (order.idPurchase)}
-        <tr>
-          <td class="border px-4 py-2">{order.collectTime}</td>
-          <td class="border px-4 py-2">{order.restaurant.companyName}</td>
-          <td class="border px-4 py-2">{order.total}</td>
+        <tr class="hover:bg-slate-100 duration-300 ease-in-out">
+          <td class="border-b px-4 py-2">{order.collectTime}</td>
+          <td class="border-b px-4 py-2">{order.restaurant.companyName}</td>
+          <td class="border-b px-4 py-2">{order.total}</td>
         </tr>
       {/each}
     </tbody>

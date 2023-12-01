5625febac00165af5c332c7263c17129d98c5faf
Bastien <bastien.hugel@reseau.eseo.fr>
2023-12-01 14:17:39 +0100

    bug for 1 dish fix

--- front/src/routes/cart/+page.svelte
+++ front/src/routes/cart/+page.svelte
@@ -52,11 +52,13 @@
     try {
       const assignedDish = [];
       const assignedMenu = [];
-      console.log(cartData)
       cartData.forEach(item => {
         for (let i = 0; i < item.quantity; i++) {
           if (item.selectedDishes){
             for (let i = 1; i <= Object.keys(item.selectedDishes).length; i++) {
+              if(Object.keys(item.selectedDishes).length == 1){
+                i ++
+              }
               assignedDish.push({ idDish: item.selectedDishes[i].idDish });
             }
             assignedMenu.push({ idMenu: parseInt(item.id.slice(4))});
